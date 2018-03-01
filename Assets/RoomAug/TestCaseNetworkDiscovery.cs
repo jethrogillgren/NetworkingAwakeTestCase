407c54155a6271a7496809a1f932ed2e423c2a74
jethro <jethrogillgren@gmail.com>
2018-03-01 10:16:15 +0000

    Test case

---
+++ Assets/RoomAug/TestCaseNetworkDiscovery.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.Networking;
+
+public class TestCaseNetworkDiscovery : NetworkDiscovery
+{
+
+    private void Awake() {   }
+
+    private void Start()
+    {
+        Initialize();
+
+
+        //Starting as client, we will see the error repeatedly
+        StartAsClient();
+
+        //Starting as Server, we see it only on exit
+        //StartAsServer();
+    }
+
+}
---
+++ Assets/RoomAug/TestCaseNetworkManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.Networking;
+
+
+public class TestCaseNetworkManager : NetworkManager
+{
+
+    //BUG TEST CASE -  If this function is commented, it works.  If not it errors.
+    //There is no Awake in the base class NetworkManager to override.
+    private void Awake()        {    }
+
+}
